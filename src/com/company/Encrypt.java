3f1441d07b4357eb9d0c5dc7d8daf6af32241630
p-ganesh <pg304@pvsd.org>
2018-10-15 11:19:11 -0400

    ;oiuhadf

---
+++ src/com/company/Encrypt.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by pg304 on 10/15/18.
+ */
+public class Encrypt {
+    public String en = "";
+
+    public Encrypt (String a) {
+        for (int i = 0; i < a.length(); i++) {
+            int j = 0;
+            char x = a.charAt(j);
+            String en = "" + x;
+            j++;
+
+        }
+        System.out.println(en);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbreader = new Scanner(System.in);
+        System.out.println("Enter the word or phrase you would like encrypted.");
+        String encrypt = kbreader.next();
+
+        Encrypt word = new Encrypt(encrypt);
+
+
+       /* System.out.println("Please select a password.");
+        String pword = kbreader.next();
+
+        System.out.println("Would you like to decrypt your word");
+        String
+        if ()
+        System.out.println("Please enter your password.");
+        String enter = kbreader.next();
+
+        if (pword == enter) {
+
+        }*/
+
     }
 }
